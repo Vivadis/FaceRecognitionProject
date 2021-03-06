8c091badf88b6431aeacc8b9aecab1270e51d9d8
Vivadis <vivadi23@mail.ru>
2021-03-06 08:23:13 +0300

    Connecting a webcam for speech recognition

--- AttendanceProject.py
+++ AttendanceProject.py
@@ -24,3 +24,30 @@ def findEncodings(images):
 
 encodeListKnown = findEncodings(images)
 print('Encoding complete')
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    success, img = cap.read()
+    imgS = cv2.resize(img,(0,0),None,0.25,0.25)
+    imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+
+    facesCurFrame = face_recognition.face_locations(imgS)
+    encodeCurFrame = face_recognition.face_encodings(imgS, facesCurFrame)
+
+    for encodeFace, faceLoc in zip(encodeCurFrame, facesCurFrame):
+        matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+        faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
+        print(faceDis)
+        matchIndex = np.argmin(faceDis)
+
+        if matches[matchIndex]:
+            name = classNames[matchIndex].upper()
+            print(name)
+            y1,x2,y2,x1 = faceLoc
+            cv2.rectangle(img,(x1,y1).(x2,y2),(0,255,0),2)
+            cv2.rectangle(img,(x1,y2-35).(x2,y2),(0,255,0),cv2.FILLED)
+            cv2.putText()
+
+    cv2.imshow('Webcam', img)
+    cv2.waitKey(1)
